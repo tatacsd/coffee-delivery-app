4b6785f0647d6d54848cb837e3f9b9da03dfb180
Thays Casado <thayscasado@gmail.com>
2024-09-17 15:42:24 -0700

    feat: update theme with font settings for Baloo 2 and Roboto

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -8,19 +8,58 @@ export const defaultTheme = {
         "purple": "#8047F8",
         "purple-light": "#EBE5F9",
 
-    },
+        },
 
-    base: {
-        "base-title": "#272221",
-        "base-subtitle": "#403937",
-        "base-text": "#574F4D",
-        "base-label": "#8D8686",
-        "base-hover": "#D7D5D5",
-        "base-button": "#E6E5E5",
-        "base-input": "#EDEDED",
-        "base-card": "#F3F2F2",
-        "background": "#FAFAFA",
-        "white": "#FFFFFF",
-    }
-}
+        base: {
+            "base-title": "#272221",
+            "base-subtitle": "#403937",
+            "base-text": "#574F4D",
+            "base-label": "#8D8686",
+            "base-hover": "#D7D5D5",
+            "base-button": "#E6E5E5",
+            "base-input": "#EDEDED",
+            "base-card": "#F3F2F2",
+            "background": "#FAFAFA",
+            "white": "#FFFFFF",
+        }
+    },
+    fonts: {
+        // Font Families
+        fontFamily: {
+            title: "Baloo 2",
+            text: "Roboto",
+        },
+        // Font Sizes
+        fontSize: {
+            titleXL: "48px",
+            titleL: "32px",
+            titleM: "24px",
+            titleS: "20px",
+            titleSX: "18px",
+            textXL: "48px",
+            textL: "20px",
+            textM: "16px",
+            textS: "14px",
+            textSX: "12px",
+        },
+        // Line Heights
+        lineHeight: {
+            titleXL: "130%",
+            titleL: "130%",
+            titleM: "130%",
+            titleS: "130%",
+            titleSX: "130%",
+            textXL: "130%",
+            textL: "130%",
+            textM: "130%",
+            textS: "130%",
+            textSX: "130%",
+        },
+        // Font Weights
+        fontWeight: {
+            extrabold: 800,
+            bold: 700,
+            regular: 400,
+        },
+    },
 }
