7ca3e6f828caf15fe65e268f8686cf4adbae4d06
Thays Casado <thayscasado@gmail.com>
2024-09-19 13:24:12 -0700

    fix: update styles and clean up context
    - Fixed missing quotes in `styles.ts` for the home page.
    - Removed unused `shoppingCart` variable from useCart on home page.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { useCart } from '../../context/CartContext';
 import { HomeMainContent } from './styles';
 
 export function Home() {
-    const { coffees, increaseQuantity, decreaseQuantity, updateCart } = useCart();
+    const { coffees, shoppingCart, increaseQuantity, decreaseQuantity, updateCart } = useCart();
 
     return (
         <div>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
-export const HomeMainContent = styled.main`
-    display: 'flex';
-    gap: '3rem';
-    flex-wrap: 'wrap';
-    margin: '2rem 0';
-`
+export const HomeMainContent = styled.div`
+    display: flex;
+    gap: 3rem;
+    flex-wrap: wrap;
+    margin: 2rem 0;
+`;
